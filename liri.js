e2a99a1a0ef3b53e2b47b05939dc4e4195c97021
Ovieh <ovieh.mosley@gmail.com>
2017-10-18 23:23:52 -0400

    removed redundant code.

--- liri.js
+++ liri.js
@@ -6,6 +6,30 @@ const request = require('request');
 let choice = process.argv[2];
 let input = process.argv[3];
 
+
+const getTweets = () => {
+    const Twitter = require('twitter');
+
+
+    const client = new Twitter(getKeys.twitterKeys);
+
+    const params = {
+        screen_name: 'fauxvieh'
+    };
+    client.get('statuses/user_timeline', params, (error, tweets, response) => {
+        if (!error) {
+            console.log(tweets);
+        }
+    });
+}
+
+const getSpotify = () => {
+
+};
+
+const getMovie = () => {
+
+}
 switch (choice) {
     case "my-tweets":
         getTweets();
@@ -22,32 +46,4 @@ switch (choice) {
     default:
         console.log("Invalid user input");
         break;
-}
-
-function getTweets() {
-    const Twitter = require('twitter');
-
-    const client = new Twitter({
-        consumer_key: getKeys.twitterKeys.consumer_key,
-        consumer_secret: getKeys.twitterKeys.consumer_secret,
-        access_token_key: getKeys.twitterKeys.access_token_key,
-        access_token_secret: getKeys.twitterKeys.access_token_secret
-    });
-
-    const params = {
-        screen_name: 'fauxvieh'
-    };
-    client.get('statuses/user_timeline', params, (error, tweets, response) => {
-        if (!error) {
-            console.log(tweets);
-        }
-    });
-}
-
-let getSpotify = () => {
-
-};
-
-let getMovie = () => {
-
 }
